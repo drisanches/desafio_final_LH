755f9e352d39ab47059d0531711c05e8e3b177e9
Drielly Sanches <driellysanches.s@gmail.com>
2024-08-30 23:51:07 +0000

    Created the CTE to rename the fields of interest in the Product table (stg_erp__products.sql)

--- models/staging/erp/stg_erp__products.sql
+++ models/staging/erp/stg_erp__products.sql
@@ -1,7 +1,33 @@
 with
     renamed as (
-        select *
+        select
+            cast(productid as int) as pk_product
+            , cast(productsubcategoryid as int) as fk_product_subcategory
+            , cast(name as varchar) as product_name
+            , cast(makeflag as boolean) as is_purchased
+            , cast(finishedgoodsflag as boolean) as is_salable
+            , cast(standardcost as float) as product_cost
+            , cast(listprice as float) as list_price
+            , cast(daystomanufacture as int) as days_to_manufacture
+            , cast(productline as varchar) as product_line
+            , cast(class as varchar) as product_class
+            , cast(style as varchar) as product_style
+            --, color
+            --, productmodelid
+            --, productnumber
+            --, size
+            --, sizeunitmeasurecode
+            --, weightunitmeasurecode
+            --, weight
+            --, safetystocklevel
+            --, reorderpoint
+            --, sellstartdate
+            --, sellenddate
+            --, discontinueddate
+            --, rowguid
+            --, modifieddate
         from {{ source('erp', 'product') }}
     )
+
 select *
 from renamed
