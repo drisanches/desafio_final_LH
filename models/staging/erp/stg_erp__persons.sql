983d2f5b909dc083499bfd94f6e18407ea635040
Drielly Sanches <driellysanches.s@gmail.com>
2024-09-03 03:32:33 +0000

    PersonType var adjusted

--- models/staging/erp/stg_erp__persons.sql
+++ models/staging/erp/stg_erp__persons.sql
@@ -2,7 +2,15 @@ with
     renamed as (
         select
             cast(businessentityid as int) as pk_business_entity
-            , cast(persontype as varchar) as person_type
+            , cast(case
+                when trim(persontype) = 'SC' then 'Store'
+                when trim(persontype) = 'IN' then 'Individual'
+                when trim(persontype) = 'SP' then 'Seller'
+                when trim(persontype) = 'EM' then 'Employee'
+                when trim(persontype) = 'VC' then 'Vendor'
+                when trim(persontype) = 'GC' then 'General'
+                else null
+            end as varchar) as person_type
             , cast(namestyle as varchar) as name_style
             , cast(firstname as varchar) as first_name
             , cast(middlename as varchar) as middle_name
