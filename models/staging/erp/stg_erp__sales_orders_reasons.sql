53a113ab72861becdc50ee9b6ac28a635321bfc5
Drielly Sanches <driellysanches.s@gmail.com>
2024-09-06 18:12:30 +0000

    Staging layer for SalesOrderHeaderSalesReason created

---
+++ models/staging/erp/stg_erp__sales_orders_reasons.sql
@@ -0,0 +1,11 @@
+with
+    renamed as (
+        select
+            cast(salesorderid as int) as fk_sales_order
+            cast(salesreasonid as int) as fk_sales_reason
+            --, modifieddate
+        from {{ source('erp', 'salesorderheadersalesreason') }}
+    )
+
+select *
+from renamed
