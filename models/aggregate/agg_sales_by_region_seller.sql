ef0295061abc457660d69fd9469f306834ba4e1f
Drielly Sanches <driellysanches.s@gmail.com>
2024-09-15 04:44:29 +0000

    Fix: aggregate table corrected (sales by region and seller)

--- models/aggregate/agg_sales_by_region_seller.sql
+++ models/aggregate/agg_sales_by_region_seller.sql
@@ -2,12 +2,17 @@ with
     fct_sales as (
         select *
         from {{ ref('fct_sales_orders') }}
-        where order_status = 'Shipped'
     )
 
-    , dim_customers as (
-        select *
-        from {{ ref('dim_customers') }}
+    , stg_sales_persons as (
+        select
+            pk_person
+            , concat(first_name,
+                coalesce(concat(' ', trim(middle_name)), ''),
+                ' ',
+                last_name) as person_name
+        from {{ ref('stg_erp__persons') }}
+        where person_type = 'Seller'
     )
 
     , dim_regions as (
@@ -17,45 +22,42 @@ with
 
     , joined as (
         select
-            sales.fk_sales_person
-            , sales.fk_customer
+            sales.fk_sales_order
+            , sales.fk_sales_person
             , sales.fk_ship_address
-            , sales.fk_sales_order
-            , sales.dt_order
+            , year(sales.dt_order) as order_year
             , sales.quantity
             , sales.gross_sales
             , sales.net_sales
-            , customers.store_name
-            , customers.customer_name as seller_name
-            , regions.city_name as city
-            , regions.state_province_name as state
+            , case
+                when customers.person_name is null then 'No Seller'
+                else customers.person_name
+            end as seller_name
             , regions.country_region_name as country
         from fct_sales as sales
-        left join dim_customers as customers
-            on sales.fk_sales_person = customers.pk_customer
+        left join stg_sales_persons as customers
+            on sales.fk_sales_person = customers.pk_person
         left join dim_regions as regions
             on sales.fk_ship_address = regions.pk_address
     )
 
     , aggregated as (
         select
-            country
-            , state
-            , city
-            , seller_name
+            seller_name
+            , order_year
+            , country
             , count(distinct fk_sales_order) as total_orders
             , sum(gross_sales) as total_gross_sales
             , sum(net_sales) as total_net_sales
             , sum(quantity) as total_quantity
         from joined
         group by 
-            city
-            , state
+            seller_name
+            , order_year
             , country
-            , seller_name
         order by 
-            country
-            , seller_name
+            seller_name
+            , order_year
     )
 
 select *
